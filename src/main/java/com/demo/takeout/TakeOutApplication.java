e7c1ce6d4597904c1efa54bb4211bab3b9123d54
glm <463956643@qq.com>
2024-09-25 23:27:45 +0800

    fix

--- src/main/java/com/demo/takeout/TakeOutApplication.java
+++ src/main/java/com/demo/takeout/TakeOutApplication.java
@@ -7,9 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TakeOutApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TakeOutApplication.class, args);
     }
-
 }
