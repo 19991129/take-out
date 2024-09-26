1f74926d2b70b92db6f853db38b4d27c3fea174d
glm <463956643@qq.com>
2024-09-26 14:38:43 +0800

    ok

--- src/main/java/com/demo/takeout/test/Program.java
+++ src/main/java/com/demo/takeout/test/Program.java
@@ -2,9 +2,24 @@ package com.demo.takeout.test;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {//主线程
+
+        List<String> list = new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+        list.add("2");
+        for (int i=0; i<list.size(); i++) {
+            if ("2".equals(list.get(i))) {
+                list.remove(i);
+                i--;
+            }
+        }
+        System.out.println(list);
+
         System.out.println(
                 LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss.SSS")) + "-" +
                         Thread.currentThread().getName());
