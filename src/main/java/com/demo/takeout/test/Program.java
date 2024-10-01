137db55a805da87f98dbcd6abceb04fb21e1565e
glm <463956643@qq.com>
2024-10-01 23:53:52 +0800

    1

--- src/main/java/com/demo/takeout/test/Program.java
+++ src/main/java/com/demo/takeout/test/Program.java
@@ -1,27 +1,10 @@
 package com.demo.takeout.test;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Program {
-    public static void main(String[] args) {//主线程
+    public static void main(String[] args) {
 
-        List<String> list = new ArrayList<String>();
-        list.add("1");
-        list.add("2");
-        list.add("2");
-        for (int i=0; i<list.size(); i++) {
-            if ("2".equals(list.get(i))) {
-                list.remove(i);
-                i--;
-            }
-        }
-        System.out.println(list);
-
-        System.out.println(
-                LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss.SSS")) + "-" +
-                        Thread.currentThread().getName());
     }
 }
